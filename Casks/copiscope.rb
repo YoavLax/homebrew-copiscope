3a7665f969c2e3eb786f970145ee7cd42b980250
cordwainersmith <kent.rage@gmail.com>
2026-05-12 11:53:53 +0300

    copiscope 1.0.1

--- Casks/copiscope.rb
+++ Casks/copiscope.rb
@@ -1,6 +1,6 @@
 cask "copiscope" do
-  version "1.0.0"
-  sha256 "a1a05cdff318538395a235c308ba9f947b718181c1b073a9437bb720e75fbe74"
+  version "1.0.1"
+  sha256 "3dabc32edfe29ff33445cc2a06edc4daf963ab2cbd586b8399f2fbb0c100f4a2"
 
   url "https://github.com/YoavLax/Copiscope/releases/download/v#{version}/Copiscope.dmg"
   name "Copiscope"
